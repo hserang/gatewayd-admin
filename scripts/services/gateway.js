5857e31efacfd8b20f8ca9c0d2bc07816bae020e
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-25 21:59:28 -0700

    [FEATURE] Add files

---
+++ scripts/services/gateway.js
@@ -0,0 +1,18 @@
+angular.module('publicApp').service('GatewayService', ['$http', function($http) {
+
+  function updateSettings() {
+    $http.get("/api/v1/settings").success(function(resp) {
+      console.log('got the settings');
+      settings.hotWallet = resp.settings.hotWallet;
+    });
+  }
+
+  var settings = {
+    hotWallet: false,
+    updateSettings: updateSettings 
+  }
+
+  updateSettings();
+
+  return settings;
+}])
