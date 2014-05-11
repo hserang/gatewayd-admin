1b1a8738897020cd215ed5cc89f2838eb5859b03
Abiy Seifu <abiy@ripple.com>
2014-05-11 16:53:52 -0700

    [CHORE] login creds

--- js/controllers/login_controller.js
+++ js/controllers/login_controller.js
@@ -1,11 +1,9 @@
 rippleGatewayApp.controller('LoginCtrl', ['$scope', 'UserService', '$location', function($scope, $user, $location) {
 
   $scope.login = function() {
-//    var password = $scope.user.password;
-//    var name = $scope.user.name;
+    var password = $scope.user.password;
+    var name = $scope.user.name;
 
-    var password = '512783f7fcf7d41474b1a7e9724ef57a80f1bfcd8fc797e172a89baaff0a4e22';
-    var name = 'admin@example.com';
     $user.login(name, password, function(err, user) {
       
       if (err) {
