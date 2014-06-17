56b274870a7a207b82bf08fbfd9bb7d62c5c2d13
Abiy Seifu <abiy@ripple.com>
2014-06-16 17:25:53 -0700

    [CHORE] store setup status in rootscope

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,12 +1,15 @@
-rippleGatewayApp.controller('MainCtrl', ['$scope', '$location', 'UserService', 'ApiService', function($scope, $location, $user, $api) {
+rippleGatewayApp.controller('MainCtrl', ['$scope', '$rootScope',  '$location', 'UserService', 'ApiService', function($scope, $rootScope, $location, $user, $api) {
   $scope.$on('$routeChangeStart', function(next, current) {
+    $rootScope.setupComplete = false;
 
     $api.setupStatus(function(error, response){
       if(error || !response.success){
         console.log('setup status error', error);
       } else if (!response.setup_complete) {
+          $rootScope.setupComplete = response.setup_complete;
           $location.path('/setup');
       } else if (!$user.isLogged && $location.path() != '/register') {
+          $rootScope.setupComplete = true;
           $location.path("/login");
       }
     });
