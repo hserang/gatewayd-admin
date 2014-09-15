f1ed37e1df9eafe7109fbcc4cb3357bea921640e
Rod Calumpong <rod@ripple.com>
2014-09-15 15:15:27 -0700

    [TASK] Implement remaining views: Gateway Transactions, KYC Data, Policies, Ripple Addresses, Ripple Transactions, Users

--- js/controllers/ripple_addresses_controller.js
+++ js/controllers/ripple_addresses_controller.js
@@ -1,3 +1,8 @@
 rippleGatewayApp.controller('RippleAddressesCtrl', [
-  '$scope', function($scope) {
+  '$scope',
+  'UserService',
+  'ApiService', function($scope, $user, $api) {
+    if (!$user.isAdmin) {  $location.path('/login') };
+
+    $scope.addresses = [];
 }]);
